b27e38e35eba4d0eb4154d171e0214246d8cf969
Nick McCurdy <nick@nickmccurdy.com>
2017-09-30 06:16:35 -0400

    Use Node 8 for better native syntax and libraries

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const JSDOM = require('jsdom').JSDOM
-const URL = require('whatwg-url').URL
+const { JSDOM } = require('jsdom')
+const { URL } = require('url')
 
 /*
  * From https://github.com/unpkg/unpkg-website/blob/c49efe2de1fa4bd673999d607f0df73b374ba4e7/server/utils/parsePackageURL.js#L3
@@ -7,17 +7,14 @@ const URL = require('whatwg-url').URL
  */
 const URLFormat = /^\/((?:@[^\/@]+\/)?[^\/@]+)(?:@([^\/]+))?(\/.*)?$/
 
-exports.list = path => {
-  return JSDOM.fromFile(path).then(dom => {
-    const nodes = Array.from(dom.window.document.querySelectorAll('link[rel=stylesheet]'))
+exports.list = async path => {
+  const dom = await JSDOM.fromFile(path)
+  const nodes = [...dom.window.document.querySelectorAll('link[rel=stylesheet]')]
 
-    return nodes.reduce((memo, node) => {
-      const pathname = new URL(node.href).pathname
-      const match = URLFormat.exec(pathname)
-      const name = match[1]
-      const version = match[2]
+  return nodes.reduce((memo, node) => {
+    const pathname = new URL(node.href).pathname
+    const [name, version] = URLFormat.exec(pathname).slice(1)
 
-      return Object.assign({}, memo, { [name]: version })
-    }, {})
-  })
+    return { ...memo, [name]: version }
+  }, {})
 }
--- spec/indexSpec.js
+++ spec/indexSpec.js
@@ -1,9 +1,5 @@
 const cdnm = require('..')
 
-it('list', () => {
-  return cdnm.list('fixture.html').then(actual => {
-    expect(actual).toEqual({
-      'normalize.css': '6.0.0'
-    })
-  })
+it('list', async () => {
+  expect(await cdnm.list('fixture.html')).toEqual({ 'normalize.css': '6.0.0' })
 })
