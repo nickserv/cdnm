6ad2d7a976130bd2e6584b9dabd9ddef20ab158f
Nick McCurdy <nick@nickmccurdy.com>
2017-10-02 04:00:42 -0400

    Add update function

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-const { JSDOM: { fromFile } } = require('jsdom')
+const fs = require('fs')
+const { JSDOM: { fromFile} } = require('jsdom')
+const { run } = require('npm-check-updates')
+const { promisify } = require('util')
 const { URL } = require('url')
+
+const writeFile = promisify(fs.writeFile)
 
 /*
  * From https://github.com/unpkg/unpkg-website/blob/c49efe2de1fa4bd673999d607f0df73b374ba4e7/server/utils/parsePackageURL.js#L3
@@ -9,12 +14,27 @@ const URLFormat = /^\/((?:@[^\/@]+\/)?[^\/@]+)(?:@([^\/]+))?(\/.*)?$/
 
 exports.list = async path => {
   const dom = await fromFile(path)
-  const links = [...dom.window.document.querySelectorAll('link[rel=stylesheet]')]
+  const links = dom.window.document.querySelectorAll('link[rel=stylesheet]')
 
-  return links.reduce((memo, link) => {
+  return Array.from(links).reduce((memo, link) => {
     const pathname = new URL(link.href).pathname
-    const [name, version] = URLFormat.exec(pathname).slice(1)
+    const [, name, version] = URLFormat.exec(pathname)
 
     return { ...memo, [name]: version }
   }, {})
+}
+
+exports.update = async path => {
+  const dom = await fromFile(path)
+  const links = dom.window.document.querySelectorAll('link[rel=stylesheet]')
+
+  for (const link of links) {
+    const url = new URL(link.href)
+    const [, name, version, file] = URLFormat.exec(url.pathname)
+    const newVersion = Object.values(await run({ packageData: JSON.stringify({ dependencies: { [name]: version } }) }))[0]
+    url.pathname = `/${name}@${newVersion}${file}`
+    link.href = url.toString()
+  }
+
+  await writeFile(path, dom.serialize())
 }
--- index.test.js
+++ index.test.js
@@ -1,5 +1,38 @@
 const cdnm = require('.')
+const fs = require('fs')
+const { promisify } = require('util')
 
-test('list', async () => {
-  await expect(cdnm.list('fixture.html')).resolves.toEqual({ 'normalize.css': '6.0.0' })
+const file = 'fixture.html'
+
+const pkg = {
+  name: 'juggernaut',
+  version: '2.1.0',
+  newVersion: '2.1.1',
+  file: '/index.js'
+}
+
+test('list', async() => {
+  await expect(cdnm.list(file)).resolves.toEqual({ [pkg.name]: pkg.version })
+})
+
+test('update', async () => {
+  const copyFile = promisify(fs.copyFile)
+  const readFile = promisify(fs.readFile)
+  const unlink = promisify(fs.unlink)
+
+  async function readHTML (file) {
+    const contents = await readFile(file, 'utf8')
+    return contents
+      .replace('DOCTYPE', 'doctype')
+      .replace(/\n/g, '')
+      .replace(/>\s+</g, '><')
+  }
+
+  const fileTmp = 'fixture_tmp.html'
+
+  await copyFile(file, fileTmp)
+  await cdnm.update(fileTmp)
+  const expected = (await readHTML(file)).replace(pkg.version, pkg.newVersion)
+  await expect(readHTML(fileTmp)).resolves.toEqual(expected)
+  await unlink(fileTmp)
 })
