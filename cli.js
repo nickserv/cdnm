10fe60ce07f67310ebf59c9158381893804be61b
Nick McCurdy <nick@nickmccurdy.com>
2017-10-13 03:33:37 -0400

    Fix file operations in CLI (fix #16)

--- cli.js
+++ cli.js
@@ -1,18 +1,19 @@
 #!/usr/bin/env node
 
-const program = require('commander')
 const cdnm = require('.')
+const program = require('commander')
+const { readFileSync, writeFileSync } = require('fs')
 const { version } = require('./package')
 
 program
   .command('list <path>')
   .description('list CDN dependencies in HTML file')
-  .action(async path => console.log(await cdnm.list(path)))
+  .action(path => console.log(cdnm.list(readFileSync(path, 'utf8'))))
 
 program
   .command('update <path>')
   .description('update CDN dependencies in HTML file')
-  .action(cdnm.update)
+  .action(async path => writeFileSync(path, await cdnm.update(readFileSync(path, 'utf8'))))
 
 program
   .description('CDN Manager')
