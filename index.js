687a6074644a73fd2b9c2beba7187cb3967b2e6b
Nick McCurdy <nick@nickmccurdy.com>
2017-10-13 03:17:29 -0400

    Use RegExp searches instead of parsing HTML

--- index.js
+++ index.js
@@ -1,74 +1,30 @@
-const { readFile, writeFile } = require('fs')
 const { run } = require('npm-check-updates')
-const { parse, serialize } = require('parse5')
-const { URL } = require('url')
-const { promisify } = require('util')
-
-const readFileAsync = path => promisify(readFile)(path, 'utf8')
-const writeFileAsync = promisify(writeFile)
 
 /*
  * From https://github.com/unpkg/unpkg-website/blob/c49efe2de1fa4bd673999d607f0df73b374ba4e7/server/utils/parsePackageURL.js#L3
  * TODO: wait until the unpkg project picks an open source license and mention it here
  */
-const URLFormat = /^\/((?:@[^/@]+\/)?[^/@]+)(?:@([^/]+))?(\/.*)?$/
+const URLFormat = /https?:\/\/unpkg.com\/((?:@[^/@]+\/)?[^/@]+)(?:@([^/]+))?(\/.*)?/g
 
-exports.createURL = url => {
-  try {
-    return new URL(url)
-  } catch (error) {
-    return null
-  }
-}
+exports.list = html => {
+  const matches = html.match(URLFormat) || []
 
-exports.findDependencies = node => {
-  if (node.nodeName === 'link' || node.nodeName === 'script') {
-    return [node]
-  } else if (node.childNodes) {
-    return Array.prototype.concat.apply([], node.childNodes.map(exports.findDependencies))
-  } else {
-    return []
-  }
-}
-
-exports.list = async path => {
-  const dom = parse(await readFileAsync(path))
-
-  return exports.findDependencies(dom).reduce((memo, dependency) => {
-    const property = dependency.attrs.find(attr => attr.name === exports.urlProperty(dependency))
-    const url = exports.createURL(property.value)
-
-    if (url && url.hostname === 'unpkg.com' && URLFormat.exec(url.pathname)) {
-      const [, name, version] = URLFormat.exec(url.pathname)
-      return { ...memo, [name]: version }
-    } else return memo
+  return matches.reduce((memo, dependency) => {
+    const [, name, version] = URLFormat.exec(dependency)
+    return { ...memo, [name]: version }
   }, {})
 }
 
-exports.update = async path => {
-  const dom = parse(await readFileAsync(path))
-  await Promise.all(exports.findDependencies(dom).map(exports.updateDependency))
-  await writeFileAsync(path, serialize(dom))
+exports.update = async html => {
+  const dependencies = exports.list(html)
+
+  const updatedDependencies = (await run({
+    jsonAll: true,
+    packageData: JSON.stringify({ dependencies })
+  })).dependencies
+
+  return html.replace(URLFormat, (match, name, version, file) => {
+    const newVersion = updatedDependencies[name]
+    return `https://unpkg.com/${name !== undefined ? name : ''}${newVersion !== undefined ? '@' + newVersion : ''}${file !== undefined ? file : ''}`
+  })
 }
-
-exports.updateDependency = async dependency => {
-  const property = dependency.attrs.find(attr => attr.name === exports.urlProperty(dependency))
-  const url = exports.createURL(property.value)
-
-  if (url && url.hostname === 'unpkg.com' && URLFormat.exec(url.pathname)) {
-    const [, name, version, file] = URLFormat.exec(url.pathname)
-
-    const dependencies = (await run({
-      jsonAll: true,
-      packageData: JSON.stringify({ dependencies: { [name]: version } })
-    })).dependencies
-
-    const newVersion = Object.values(dependencies)[0]
-    url.pathname = `/${name !== undefined ? name : ''}${newVersion !== undefined ? '@' + newVersion : ''}${file !== undefined ? file : ''}`
-    property.value = url.toString()
-  }
-
-  return dependency
-}
-
-exports.urlProperty = element => ({ link: 'href', script: 'src' }[element.nodeName])
--- index.test.js
+++ index.test.js
@@ -1,131 +1,89 @@
 const cdnm = require('.')
-const { copyFile, readFile, unlink } = require('fs')
-const { parse, parseFragment, serialize } = require('parse5')
-const { URL } = require('url')
-const { promisify } = require('util')
-
-// Async functions
-const copyFileAsync = promisify(copyFile)
-const readFileAsync = path => promisify(readFile)(path, 'utf8')
-const readHTML = async file => (
-  (await readFileAsync(file))
-    .replace('DOCTYPE', 'doctype')
-    .replace(/\n/g, '')
-    .replace(/>\s+</g, '><')
-)
-const unlinkAsync = promisify(unlink)
-
-// AST nodes
-const parseNode = node => parseFragment(node).childNodes[0]
-const createLink = href => parseNode(`<link  href="${href}" rel="stylesheet">`)
-const createScript = src => parseNode(`<script src=${src}></script>`)
-
-// Test files
-const file = 'fixture.html'
-const fileTmp = 'fixture_tmp.html'
+const { readFileSync } = require('fs')
 
 // Package metadata
+const html = readFileSync('fixture.html', 'utf8')
 const name = 'juggernaut'
 const version = '2.1.0'
 const newVersion = '2.1.1'
-const unpkgURL = `https://unpkg.com/${name}`
 const replaceVersion = string => string.replace(version, newVersion)
 
-describe('createURL', () => {
-  test('valid URL', () => expect(cdnm.createURL(unpkgURL)).toEqual(new URL(unpkgURL)))
-  test('invalid URL', () => expect(cdnm.createURL('')).toBe(null))
+describe('list', () => {
+  test('empty string', () => {
+    expect(cdnm.list('')).toEqual({})
+  })
+
+  test('complete html document', () => {
+    expect(cdnm.list(html)).toEqual({ [name]: version })
+  })
 })
 
-test('findDependencies', async () => {
-  const node = parse(await readFileAsync(file))
-  const serializeArray = array => array.map(serialize)
-  expect(serializeArray(cdnm.findDependencies(node))).toEqual(serializeArray([createScript(unpkgURL)]))
-})
+describe('update', () => {
+  test('empty string', async () => {
+    expect(await cdnm.update('')).toEqual('')
+  })
 
-test('list', async () => expect(await cdnm.list(file)).toEqual({ [name]: version }))
+  test('complete html document', async () => {
+    expect(await cdnm.update(html)).toBe(replaceVersion(html))
+  })
 
-test('update', async () => {
-  await copyFileAsync(file, fileTmp)
-  await cdnm.update(fileTmp)
-
-  expect(await readHTML(fileTmp)).toBe(replaceVersion(await readHTML(file)))
-
-  await unlinkAsync(fileTmp)
-})
-
-describe('updateDependency', () => {
-  test('script with fixed version', async () => {
+  test('fixed version', async () => {
     const url = `https://unpkg.com/${name}@${version}/index.js`
-    expect(await cdnm.updateDependency(createScript(url))).toEqual(createScript(replaceVersion(url)))
+    expect(await cdnm.update(url)).toBe(replaceVersion(url))
   })
 
-  test('script with latest version', async () => {
-    const script = createScript(`https://unpkg.com/${name}@${newVersion}/index.js`)
-    expect(await cdnm.updateDependency(script)).toEqual(script)
+  test('latest version', async () => {
+    const url = `https://unpkg.com/${name}@${newVersion}/index.js`
+    expect(await cdnm.update(url)).toBe(url)
   })
 
-  test('script with semver range', async () => {
-    const script = createScript(`https://unpkg.com/${name}@^${newVersion}/index.js`)
-    expect(await cdnm.updateDependency(script)).toEqual(script)
+  test('semver range', async () => {
+    const url = `https://unpkg.com/${name}@^${newVersion}/index.js`
+    expect(await cdnm.update(url)).toBe(url)
   })
 
-  test('script with tag', async () => {
-    const script = createScript(`https://unpkg.com/${name}@latest/index.js`)
-    expect(await cdnm.updateDependency(script)).toEqual(script)
+  test('tag', async () => {
+    const url = `https://unpkg.com/${name}@latest/index.js`
+    expect(await cdnm.update(url)).toBe(url)
   })
 
-  test('script without vesion', async () => {
-    const script = createScript(`https://unpkg.com/${name}/index.js`)
-    expect(await cdnm.updateDependency(script)).toEqual(script)
+  test('without vesion', async () => {
+    const url = `https://unpkg.com/${name}/index.js`
+    expect(await cdnm.update(url)).toBe(url)
   })
 
-  test('script without path', async () => {
+  test('without path', async () => {
     const url = `https://unpkg.com/${name}@${version}`
-    expect(await cdnm.updateDependency(createScript(url))).toEqual(createScript(replaceVersion(url)))
+    expect(await cdnm.update(url)).toBe(replaceVersion(url))
   })
 
-  test('script with trailing slash', async () => {
+  test('trailing slash', async () => {
     const url = `https://unpkg.com/${name}@${version}/`
-    expect(await cdnm.updateDependency(createScript(url))).toEqual(createScript(replaceVersion(url)))
+    expect(await cdnm.update(url)).toBe(replaceVersion(url))
   })
 
-  test('script for home page', async () => {
-    const script = createScript(`https://unpkg.com`)
-    expect(await cdnm.updateDependency(script)).toEqual(script)
+  test('home page', async () => {
+    const url = `https://unpkg.com`
+    expect(await cdnm.update(url)).toBe(url)
   })
 
-  test('script with query', async () => {
+  test('query', async () => {
     const url = `https://unpkg.com/${name}@${version}/index.js?main=example`
-    expect(await cdnm.updateDependency(createScript(url))).toEqual(createScript(replaceVersion(url)))
-  })
-
-  test('link', async () => {
-    const link = createLink(unpkgURL)
-    expect(await cdnm.updateDependency(link)).toEqual(link)
-  })
-
-  test('any link rel', async () => {
-    const html = `<link href="${unpkgURL}" rel="invalid">`
-    expect(await cdnm.updateDependency(parseNode(html))).toEqual(parseNode(replaceVersion(html)))
+    expect(await cdnm.update(url)).toBe(replaceVersion(url))
   })
 
   test('absolute href', async () => {
-    const script = createScript('https://example.com/index.js')
-    expect(await cdnm.updateDependency(script)).toEqual(script)
+    const url = 'https://example.com/index.js'
+    expect(await cdnm.update(url)).toBe(url)
   })
 
   test('relative href', async () => {
-    const script = createScript('index.js')
-    expect(await cdnm.updateDependency(script)).toEqual(script)
+    const url = 'index.js'
+    expect(await cdnm.update(url)).toBe(url)
   })
 
   test('root relative href', async () => {
-    const script = createScript('/index.js')
-    expect(await cdnm.updateDependency(script)).toEqual(script)
+    const url = '/index.js'
+    expect(await cdnm.update(url)).toBe(url)
   })
-})
-
-describe('urlProperty', () => {
-  test('link', () => expect(cdnm.urlProperty(createLink(unpkgURL))).toBe('href'))
-  test('script', () => expect(cdnm.urlProperty(createScript(unpkgURL))).toBe('src'))
 })
