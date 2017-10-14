461e950d13efda505f8a15521b3eee446fdf5d9f
Nick McCurdy <nick@nickmccurdy.com>
2017-10-14 06:48:37 -0400

    Refactor tests with shared behaviors (fix #11)

--- index.test.js
+++ index.test.js
@@ -9,81 +9,26 @@ const newVersion = '2.1.1'
 const replaceVersion = string => string.replace(version, newVersion)
 
 describe('list', () => {
-  test('empty string', () => {
-    expect(cdnm.list('')).toEqual({})
-  })
-
-  test('complete html document', () => {
-    expect(cdnm.list(html)).toEqual({ [name]: version })
-  })
+  test('empty string', () => expect(cdnm.list('')).toEqual({}))
+  test('complete html document', () => expect(cdnm.list(html)).toEqual({ [name]: version }))
 })
 
 describe('update', () => {
-  test('empty string', async () => {
-    expect(await cdnm.update('')).toEqual('')
-  })
+  const expectToUpdate = url => async () => expect(await cdnm.update(url)).toBe(replaceVersion(url))
+  const expectNotToUpdate = url => async () => expect(await cdnm.update(url)).toBe(url)
 
-  test('complete html document', async () => {
-    expect(await cdnm.update(html)).toBe(replaceVersion(html))
-  })
-
-  test('fixed version', async () => {
-    const url = `https://unpkg.com/${name}@${version}/index.js`
-    expect(await cdnm.update(url)).toBe(replaceVersion(url))
-  })
-
-  test('latest version', async () => {
-    const url = `https://unpkg.com/${name}@${newVersion}/index.js`
-    expect(await cdnm.update(url)).toBe(url)
-  })
-
-  test('semver range', async () => {
-    const url = `https://unpkg.com/${name}@^${newVersion}/index.js`
-    expect(await cdnm.update(url)).toBe(url)
-  })
-
-  test('tag', async () => {
-    const url = `https://unpkg.com/${name}@latest/index.js`
-    expect(await cdnm.update(url)).toBe(url)
-  })
-
-  test('without vesion', async () => {
-    const url = `https://unpkg.com/${name}/index.js`
-    expect(await cdnm.update(url)).toBe(url)
-  })
-
-  test('without path', async () => {
-    const url = `https://unpkg.com/${name}@${version}`
-    expect(await cdnm.update(url)).toBe(replaceVersion(url))
-  })
-
-  test('trailing slash', async () => {
-    const url = `https://unpkg.com/${name}@${version}/`
-    expect(await cdnm.update(url)).toBe(replaceVersion(url))
-  })
-
-  test('home page', async () => {
-    const url = `https://unpkg.com`
-    expect(await cdnm.update(url)).toBe(url)
-  })
-
-  test('query', async () => {
-    const url = `https://unpkg.com/${name}@${version}/index.js?main=example`
-    expect(await cdnm.update(url)).toBe(replaceVersion(url))
-  })
-
-  test('absolute href', async () => {
-    const url = 'https://example.com/index.js'
-    expect(await cdnm.update(url)).toBe(url)
-  })
-
-  test('relative href', async () => {
-    const url = 'index.js'
-    expect(await cdnm.update(url)).toBe(url)
-  })
-
-  test('root relative href', async () => {
-    const url = '/index.js'
-    expect(await cdnm.update(url)).toBe(url)
-  })
+  test('empty string', expectNotToUpdate(''))
+  test('complete html document', expectToUpdate(html))
+  test('fixed version', expectToUpdate(`https://unpkg.com/${name}@${version}/index.js`))
+  test('latest version', expectNotToUpdate(`https://unpkg.com/${name}@${newVersion}/index.js`))
+  test('semver range', expectNotToUpdate(`https://unpkg.com/${name}@^${newVersion}/index.js`))
+  test('tag', expectNotToUpdate(`https://unpkg.com/${name}@latest/index.js`))
+  test('without vesion', expectNotToUpdate(`https://unpkg.com/${name}/index.js`))
+  test('without path', expectToUpdate(`https://unpkg.com/${name}@${version}`))
+  test('trailing slash', expectToUpdate(`https://unpkg.com/${name}@${version}/`))
+  test('home page', expectNotToUpdate('https://unpkg.com'))
+  test('query', expectToUpdate(`https://unpkg.com/${name}@${version}/index.js?main=example`))
+  test('absolute href', expectNotToUpdate('https://example.com/index.js'))
+  test('relative href', expectNotToUpdate('index.js'))
+  test('root relative href', expectNotToUpdate('/index.js'))
 })
